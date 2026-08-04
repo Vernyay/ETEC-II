8d4f65319dbaa0dbf66caafbc16aa99ead4cca8e
Willian Raphael <willraphaelcarvalhodas@gmail.com>
2026-08-03 21:39:03 -0300

    Create Tela_Exercício1.php with HTML form

---
+++ SW/Tela_Exercício1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela 1</title>
+</head>
+<body>
+    <form action="impar.php" method="post">
+        <input type="number" name="numero" id="numero"> <br>
+        <input type="submit" name="btnMethod" id="btnMethod">
+    </form>
+</body>
+</html>
